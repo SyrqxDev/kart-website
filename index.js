8edc3eb33367e06d08a90856bf93974a35c6f073
SyrqxDev <67019699+SyrqxDev@users.noreply.github.com>
2025-06-25 16:07:37 +0100

    updated home

--- index.js
+++ index.js
@@ -64,7 +64,9 @@ const images = [
     { url: '/assets/img/img6.jpeg', position: 'center center' },
     { url: '/assets/img/img7.jpeg', position: 'center center' },
     { url: '/assets/img/img8.jpeg', position: 'center center' },
-
+    { url: '/assets/img/img9.jpg', position: 'center center' },
+    { url: '/assets/img/img10.jpg', position: 'center center' },
+    { url: '/assets/img/img11.jpg', position: 'center center' },
 ];
 
 const slideshowContainer = document.getElementById('slideshow');
